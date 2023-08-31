a9b68791496b684cf9fa699d0c57119c8de20ed0
kiumars_ch <kiumarsch96@gmail.com>
2023-08-31 19:11:04 +0330

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by kiumars on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
